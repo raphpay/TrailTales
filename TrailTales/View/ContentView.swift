accdeafd6a67eb3faff3fb025e9a96535b6b245e
Raphaël Payet <raphpay.dev@gmail.com>
2023-09-11 11:38:00 +0200

    Feature: Implement navigation from start based on login status #3

--- TrailTales/View/ContentView.swift
+++ TrailTales/View/ContentView.swift
@@ -6,23 +6,42 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct ContentView: View {
     @AppStorage(AppStorageConstants.hasOpenedAppBefore.rawValue) var hasOpenedAppBefore: Bool = false
-
+    @State private var isLoggedIn: Bool = false // Add a state variable to track user login status
+    
     var body: some View {
         NavigationView {
             if hasOpenedAppBefore {
-                // User has opened the app before, navigate directly to the Login screen.
-                LoginView()
+                // User has opened the app before, check login status
+                if isLoggedIn {
+                    // User is logged in, navigate to DashboardView
+                    DashboardView()
+                } else {
+                    // User is not logged in, navigate to LoginView
+                    LoginView()
+                }
             } else {
                 // Welcome screen should only appear if the user never opened the app.
                 WelcomeView()
             }
         }
+        .onAppear {
+            // Check Firebase authentication status when the app loads
+            checkAuthStatus()
+        }
+    }
+    
+    // Function to check Firebase authentication status
+    private func checkAuthStatus() {
+        if Auth.auth().currentUser != nil {
+            // User is authenticated, update the isLoggedIn state
+            isLoggedIn = true
+        }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
