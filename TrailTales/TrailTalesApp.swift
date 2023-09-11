affa454cac988a4189a52d4961174e13a1c26e05
Raphaël Payet <raphpay.dev@gmail.com>
2023-09-11 10:31:40 +0200

    First commit

---
+++ TrailTales/TrailTalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailTalesApp.swift
+//  TrailTales
+//
+//  Created by Raphaël Payet on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrailTalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
