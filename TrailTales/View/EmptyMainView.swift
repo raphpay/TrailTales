72bdb924335d147d6880f12d106970d7f3e843a4
Raphaël Payet <raphpay.dev@gmail.com>
2023-09-20 16:55:41 +0200

    Feature: Add app icon #6

---
+++ TrailTales/View/EmptyMainView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyMainView.swift
+//  TrailTales
+//
+//  Created by Raphaël Payet on 20/09/2023.
+//
+
+import SwiftUI
+
+struct EmptyMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmptyMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyMainView()
+    }
+}
